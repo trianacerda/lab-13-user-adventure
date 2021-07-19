915cc3fe61a49eb4e30ee4510fa66e17c29bb5b0
Triana Cerda <trianaRcerda@gmail.com>
2021-07-19 14:45:20 -0700

    added files and folders + working on findById, set, get LS info functions

---
+++ home/home.js
@@ -0,0 +1,15 @@
+import quests from '../data/quest-data.js';
+
+const homeForm = document.getElementById('home-form');
+
+homeForm.addEventListener('submit', e => {
+    e.preventDefault(); 
+
+    const formData = new FormData(homeForm); 
+    const newUser = 
+    //store this information in LS 
+
+});
+
+
+
