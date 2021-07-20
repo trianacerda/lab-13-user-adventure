f703ef30120dbe2e48306cd92e20950c41f6c0e9
Triana Cerda <trianaRcerda@gmail.com>
2021-07-19 17:56:22 -0700

    genie and witch pic is visible -- still need mermaid pic

--- quest/quest.js
+++ quest/quest.js
@@ -4,7 +4,7 @@ import { findById } from '../data/findById.js';
 const searchParams = new URLSearchParams(window.location.search);
 
 const questTitle = document.getElementById('title');
-const questImg = document.getElementById('img');
+const questImg = document.getElementById('quest-img');
 const description = document.getElementById('description');
 const choices = document.getElementById('choice');
 
@@ -12,7 +12,7 @@ const quest = findById(quests, searchParams.get('questId'));
 
 questTitle.textContent = quest.title; 
 description.textContent = quest.description;
-questImg.src = quest.image;
+questImg.src = `../assets/${quest.image}`;
 
 for (let choice of quest.choice){
     const label = document.createElement('label');
