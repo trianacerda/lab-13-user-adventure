ee8a93b454585a72e7de7ddabc4727cb0195d053
Triana Cerda <trianaRcerda@gmail.com>
2021-07-20 17:42:28 -0700

    need to work on results page. tests passing and page functioning

--- quest/quest.js
+++ quest/quest.js
@@ -1,16 +1,17 @@
 import quests from '../data/quest-data.js';
 import { findById } from '../data/findById.js';
+import { setLs, getLs } from '../data/storage-utils.js';
 
 const searchParams = new URLSearchParams(window.location.search);
 
 const questTitle = document.getElementById('title');
 const questImg = document.getElementById('quest-img');
 const description = document.getElementById('description');
-const choices = document.getElementById('pick-form');
+const choices = document.getElementById('choices');
 
 const quest = findById(quests, searchParams.get('questId'));
 
-questTitle.textContent = quest.title; 
+questTitle.textContent = quest.title;
 description.textContent = quest.description;
 questImg.src = `../assets/${quest.image}`;
 
@@ -30,3 +31,22 @@ for (let choice of quest.choices){
     choices.appendChild(label);
 
 }
+
+const pickForm = document.getElementById('pick-form');
+pickForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const questForm = new FormData(pickForm);
+    const choiceValue = questForm.get('pick');
+    const choiceData = findById(quest.choices, choiceValue);
+    const gameUser = getLs();
+    gameUser.jewels += choiceData.jewels;
+    gameUser.hp += choiceData.hp;
+    gameUser.completed[quest.id] = true;
+    setLs(gameUser);
+
+    const mapLink = document.getElementById('map-link');
+    description.textContent = choiceData.result;
+    pickForm.classList.add('hidden');
+    mapLink.classList.remove('hidden');
+
+});
